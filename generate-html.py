439d510e321ed5150fc50a1dcb894b7d984ff149
wuqingze <nongxiaolang@foxmail.com>
2024-02-12 18:19:59 +0800

    commit

--- generate-html.py
+++ generate-html.py
@@ -2,29 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 html_template = '''
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>英语单词页面</title>
-</head>
-<body>
-    <div>
-        <div display="flex">
-            <div><h3>{}</h3></div>
-            <div><audio controls><source src="{}" type="audio/mp3"></audio></div>
-        </div>
-        <div>{}</div>
-    </div>
-</body>
-</html>
-'''
-
-page_template = '''
 <!DOCTYPE html>
 <html>
 <head>
@@ -46,16 +24,17 @@ word_template = '''
     </div>
 '''
 
-words = ["Academic", "Endorsement"]
-
+words = ["Academic", "Endorsement", "Apple","easy-going"]
+word_html_list = []
+audio_prefix_url = "https://api.frdic.com/api/v2/speech/speakweb?"
+word_prefix_url = "https://beta_dict.eudic.net/dicts/en/"
 for word in words:
-    audio_url = "https://api.frdic.com/api/v2/speech/speakweb?"
+    word_url = word_prefix_url+word
     chinese_defenition = ""
-
-    url = "https://beta_dict.eudic.net/dicts/en/"+word
+    audio_url = ""
 
     # 发送HTTP请求
-    response = requests.get(url)
+    response = requests.get(word_url)
 
     # 检查响应状态码
     if response.status_code == 200:
@@ -63,21 +42,18 @@ for word in words:
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # 使用CSS选择器定位元素
-        target_element = soup.select_one("#exp-head > div > span.phonitic-line > a:nth-child(2)")
-
+        audio_element = soup.select_one("#exp-head > div > span.phonitic-line > a:nth-child(2)")
         chinese_defenition_element = soup.select_one("#ExpFCChild")
 
         # 检查元素是否存在
-        if target_element:
+        if audio_element is not None and chinese_defenition_element is not None:
             # 获取data-rel属性值
-            data_rel_value = target_element.get("data-rel")
-        else:
-            print("未找到目标元素")
+            data_rel_value = audio_element.get("data-rel")
+            audio_url = audio_prefix_url + data_rel_value
 
-        if chinese_defenition_element.find("hr") is None:
             chinese_defenition_element.append(soup.new_tag("hr"))
-        print(page_template.format(word_template.format(word, audio_url+data_rel_value, chinese_defenition_element.prettify())))
-    else:
-        print("请求失败，状态码:", response.status_code)
+            chinese_defenition = chinese_defenition_element.prettify()
 
+    word_html_list.append(word_template.format(word, audio_url, chinese_defenition))
 
+print(html_template.format("".join(word_html_list)))
